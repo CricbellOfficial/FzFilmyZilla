707d4e39f334dcb3c458ddfc406dedd70103209e
Ganeshmrxx <100681292+Ganeshmrxx@users.noreply.github.com>
2023-02-23 09:38:00 -0800

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -82,29 +82,27 @@ async def next_page(bot, query):
         return
     settings = await get_settings(query.message.chat.id)
     if settings['button']:
-
-             btn = [
-                 [
-                    InlineKeyboardButton(
-                        text=f"[{get_size(file.file_size)}]-{file.file_name}", callback_data=f'files#{file.file_id}'
-                    ),     
-                 ]
-                 for file in files
-            
-             ]
+        btn = [
+            [
+                InlineKeyboardButton(
+                   text=f"[{get_size(file.file_size)}]-💠-{file.file_name}", callback_data=f'files#{file.file_id}'
+                ),     
+            ]
+            for file in files
+        ]
     else:
-              btn = [
-                  [
-                     InlineKeyboardButton(
-                        text=f"{myString}", callback_data=f'files#{file.file_id}'
-                     ),
-                     InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
-                         callback_data=f'files_#{file.file_id}',
-                     ),
-                  ]
-                  for file in files
-              ]
+        btn = [
+            [
+                InlineKeyboardButton(
+                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                ),
+                InlineKeyboardButton(
+                   text=f"{get_size(file.file_size)}",
+                    callback_data=f'files_#{file.file_id}',
+                ),
+            ]
+            for file in files
+        ]
  
    
     if 0 < offset <= 6:
@@ -661,17 +659,15 @@ async def auto_filter(client, msg, spoll=False):
         search, files, offset, total_results = spoll
     pre = 'filep' if settings['file_secure'] else 'file'
     if settings["button"]:
-
-             btn = [
-                [
-                     InlineKeyboardButton(
-                         text=f"[{get_size(file.file_size)}]-{await get_shortlink(f"hello")}", callback_data=f'{pre}#{file.file_id}'
-                     ),
-                ]
-                for file in files
-             ]
+        btn = [
+            [
+                InlineKeyboardButton(
+                    text=f"[{get_size(file.file_size)}]-💠-{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                ),
+            ]
+            for file in files
+        ]
     else:
-        
         btn = [
             [
                 InlineKeyboardButton(

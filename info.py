7de199c043b6193f82ab07bc45cb6b8838e493d9
Ganeshmrxx <100681292+Ganeshmrxx@users.noreply.github.com>
2023-03-13 08:14:10 -0700

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001896863763'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'fzfilmyzilla')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<code>{file_name}</code>\n\n©️ by: <a href=https://t.me/fzfilmyzilla>MRX</a>")

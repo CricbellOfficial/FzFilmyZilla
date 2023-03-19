2f0631b75fe913d7fcfc980db5f415b88541279f
Ganeshmrxx <100681292+Ganeshmrxx@users.noreply.github.com>
2023-03-19 08:28:45 -0700

    Update scdule.py

--- plugins/scdule.py
+++ plugins/scdule.py
@@ -6,13 +6,8 @@ from pyrogram.types import Message
 
 async def job():
      youtube = BeautifulSoup(requests.get(f"https://www.youtube.com/results?search_query=cricket+shorts").text, "html.parser")
-     ylink = youtube.find_all('a')['href']
-     if ylink:
-        for l in ylink:
-            if "shorts/" in l:
-                print(l)
-                await Client.send_message("888849950", f"**𝙵𝙸𝙽𝙳𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚅𝙸𝙳𝙴𝙾**")
-                break
+     await Client.send_message("888849950", f"**𝙵𝙸𝙽𝙳𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚅𝙸𝙳𝙴𝙾**")
+    
 
 
 scheduler = AsyncIOScheduler()

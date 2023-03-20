133ff371d51ed5fae211b1d6534d691dfae65398
Ganeshmrxx <100681292+Ganeshmrxx@users.noreply.github.com>
2023-03-19 22:06:50 -0700

    Update scdule.py

--- plugins/scdule.py
+++ plugins/scdule.py
@@ -10,16 +10,7 @@ from telethon.tl.types import InputMediaPoll, Poll, PollAnswer
 from telethon.sessions import StringSession
 
 
-apiid = API_ID
-apihash = API_HASH
-bottoken = BOT_TOKEN
-ss = "1BVtsOLgBuyDLMoZOURUGEg81gV5njgorTVbwoEAuJV4w0SCGIH4zeyU5rTGIRTdqXmCH7iASI7g7WybmbKqLdrQs1jADUDxgrI2dy5t5X7Te3O8mu7zLkqyW7ui2pSLnk1xYpGRBm9Fs6lbGyCugpioMREpby3xQxPngJzo8loqiCqFDft_s95GTNr4GlroaY6DjbgH_i_LPPjon92HG94ZvoiUz9ky8Ate6TkWUNkFRrqdzt1lauIbZ1enUF-26LP-u-4VJfzZPNYp2ttyo22T-1YyIbfdrCac3kL4c-gbjDRFq-3gsYd8EIjtv4ZXVYAFn1hTtgcf_JrfkPsvp1Uz_Zrm6A98="
-     
-BotzHubUser = TelegramClient(StringSession(ss), apiid, API_HASH)
-BotzHubUser.start()
-     
-group_id = -1001909929331
-BotzHubUser.send_message(group_id, "hii")
+print("enter here)
       
 async def job():
      
@@ -44,9 +35,3 @@ async def job():
         #await BotzHubUser.send_message(group_id, newn)
         print("error")
     
-
-
-scheduler = AsyncIOScheduler()
-#scheduler.add_job(job, "interval", seconds=30)
-
-#scheduler.start()
